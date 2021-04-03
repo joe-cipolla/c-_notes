3f5ba2692d9dab0025a2651e5fb3535036e28982
Joe Cipolla <joe-cipolla@users.noreply.github.com>
2021-04-02 23:08:08 -0500

    Add files via upload

---
+++ letter.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// my first C++ program, setting goals for learning
+
+/* how to compile and run a script:
+	$ g++ source.cpp -o <name-you-want-to-give> 
+	$ ./myprogram
+*/
+
+int main()
+{
+
+  std::cout << "Dear Self,\n";
+  std::cout << "I will acheive the following goals while learning C++:\n";
+  std::cout << "1. Write a drive monitoring script.\n";
+  std::cout << "2. Investigate other C++ projects,\n";
+  std::cout << "3. Study/Write C++ 15 minutes a day.\n";
+  std::cout << "\n";
+  std::cout << "Joe Cipolla\n";
+  std::cout << "2021-04-02\n";
+
+}
